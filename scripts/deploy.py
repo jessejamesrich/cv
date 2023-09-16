8210570fe2cab7bcc96abc35bc7b044a8b563da9
Jesse James Richard <j@jessejamesrichard.com>
2023-09-16 10:35:20 -0700

    Commit message

--- scripts/deploy.py
+++ scripts/deploy.py
@@ -30,6 +30,6 @@ subprocess.run(["git", "add", "."], check=True)
 subprocess.run(["git", "commit", "-m", "Commit message"], check=True)
 
 # Push changes to the specified branch using the personal access key
-subprocess.run(["git", "push", repository_url, f"HEAD:{branch}", "--force", f"-u {personal_access_key}"], check=True)
+subprocess.run(["git", "push", repository_url, f"HEAD:{branch}", "--force"], check=True)
 
 print("Changes have been committed and pushed to the repository.")
